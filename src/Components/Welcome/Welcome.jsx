099ef7a863c1ac13d9b88f806ba189aefd437715
Akande Olalekan Toheeb <akandeolalekantoheeb94@gmail.com>
2024-06-11 16:18:10 +0100

    Built Welcome Page

--- src/Components/Welcome/Welcome.jsx
+++ src/Components/Welcome/Welcome.jsx
@@ -1,10 +1,18 @@
+import Button from "../Button/Button"
 import Logo from "../../assets/Logo.png"
 
 export default function Welcome(){
     return(
         <>
             <div className="welcome">
-                <img src={Logo} alt="lgog" />
+                 <img src={Logo} alt="logo" />
+
+                <h2>Welcome to <span className="welcome__company-Name">msme</span> Employment Scheme</h2>
+                <p>Our aim is to provide jobs for <span className="target">10,000</span> youths all over Nigeria</p>
+
+                <Button type='a' className="btn btn--welcome" to={`#`}>
+                  Get Employment
+                </Button>
             </div>
         </>
     )
